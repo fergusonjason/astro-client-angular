4403970c731889ee2eaaf80faeac8c04b70790b4
Jason Ferguson <fergusonjason@gmail.com>
2022-10-16 07:14:13 -0400

    add stellar class select box

--- src/app/modules/gliese/components/gliese-home/gliese-home.component.ts
+++ src/app/modules/gliese/components/gliese-home/gliese-home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { GlieseService } from '../../services/gliese.service';
+import { Gliese } from '../../types/gliese';
 
 @Component({
   selector: 'app-gliese-home',
@@ -11,14 +13,20 @@ export class GlieseHomeComponent implements OnInit {
 
   searchForm : FormGroup = new FormGroup({});
 
-  constructor(private formBuilder : FormBuilder, public $gliese: GlieseService) { }
+  searchResults : Gliese[] = [];
+
+  constructor(private formBuilder : FormBuilder, public $gliese: GlieseService, public $dialog : MatDialog) { }
 
   ngOnInit(): void {
 
     this.searchForm = this.formBuilder.group({
       name : new FormControl(''),
-
+      stellar_class: new FormControl('')
     });
+  }
+
+  onSearch() : void {
+
   }
 
 }
