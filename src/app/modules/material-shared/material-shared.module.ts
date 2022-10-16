d72462e9f4a5c2beabcc8abcf2900b832a1332ba
Jason Ferguson <fergusonjason@gmail.com>
2022-10-16 07:12:09 -0400

    add dialog and select module to shared

--- src/app/modules/material-shared/material-shared.module.ts
+++ src/app/modules/material-shared/material-shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { CoreModule } from '../core/core.module';
@@ -16,16 +18,20 @@ import { SharedModule } from '../shared/shared.module';
     CoreModule,
     SharedModule,
     MatButtonModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule,
 
   ],
   exports: [
     MatButtonModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule,
   ]
