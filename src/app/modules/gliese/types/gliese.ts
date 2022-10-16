78ccd14441f7ef780f78171e4b700d295c890197
Jason Ferguson <fergusonjason@gmail.com>
2022-10-16 07:11:31 -0400

    create gliese interface

---
+++ src/app/modules/gliese/types/gliese.ts
@@ -0,0 +1,19 @@
+export interface Gliese {
+
+    id : number;
+    name : string;
+    component : string;
+    ra : string;
+    dec : string;
+    proper_motion : string;
+    radial_velocity : string;
+    spectral_type_full : string;
+    spectral_type : string;
+    spectral_type_sub : string;
+    luminosity_class : string;
+    visual_magnitude : number;
+    absolute_magnitude : number;
+    parallax : number;
+    bv_color : number;
+    hd_number : number;
+}
