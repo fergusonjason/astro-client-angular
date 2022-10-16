cf041f36578f52d4f114418f932e2e7c06fc3a7b
Jason Ferguson <fergusonjason@gmail.com>
2022-10-16 07:12:56 -0400

    gliese service now extends base catalog service

--- src/app/modules/gliese/services/gliese.service.ts
+++ src/app/modules/gliese/services/gliese.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { CatalogService } from '../../shared/services/catalog.service';
 
 // @Injectable({
 //   providedIn: 'root'
 // })
-export class GlieseService {
+export class GlieseService extends CatalogService {
 
-  constructor() { }
+
+  constructor() {
+    super();
+  }
 }
